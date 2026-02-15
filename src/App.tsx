fa2e7755082f22fd2ea0451b040c2bc9cda953eb
Larissa Moreira <larissamoreira@MacBook-Air.local>
2026-02-15 01:27:37 -0300

    feat: initial navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Navbar } from './components/Navbar';
 import './index.css';
 
 function App() {
-  return <div />;
+  return (
+    <main className="min-h-screen w-full max-w-[100vw] overflow-x-hidden flex flex-col">
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
