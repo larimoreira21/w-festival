6a77cf5d513551e4bbb142ba6e0113bf646c3e4d
Larissa Moreira <larissamoreira@MacBook-Air.local>
2026-02-15 01:32:22 -0300

    feature: initial footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 import './index.css';
 
 function App() {
   return (
     <main className="min-h-screen w-full max-w-[100vw] overflow-x-hidden flex flex-col">
       <Navbar />
+
+      <div className="pt-14 w-full">
+        <Footer />
+      </div>
     </main>
   );
 }
